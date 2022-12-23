3b8740e34cadcdd858f355fa994d3727d3799621
lbcanfield <lewie.b.canfield@gmail.com>
2022-12-23 13:04:19 -0600

    created initial routes for the post-router.js

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,4 +4,32 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/', (req, res) => {
+     // finish this to get all posts in the database
+});
+
+router.get('/:id', (req, res) => {
+     // finish this to get specific post by id
+});
+
+router.post('/', (req, res) => {
+     // finish this to add a new post to the database
+});
+
+router.put('/:id', (req, res) => {
+     // finish this to update a post in the database
+});
+
+router.delete('/:id', (req, res) => {
+     // finish this to delete a post by id
+});
+
+router.get('/:id/comments', (req, res) => {
+     // finish this to get comments from a specific post
+});
+
+
+
+
+
 module.exports = router;
